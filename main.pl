84d91b3e1743c5f998e8e0a46a3e94fb32fa88d1
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-04-01 00:07:45 -0600

    posicionarPieza
    metodo que verifica si una pieza calza en la figura

--- main.pl
+++ main.pl
@@ -26,7 +26,7 @@ comparar(M1,M2,G):-
 
 
 
-valorEnPos(R, C, Mat, Upd, F) :-
+valorEnPos(R, C, Mat,F) :-
     nth0(R, Mat, OldRow, RestRows),   % get the row and the rest
     nth0(C, OldRow, _Val, NewRow),    % we don't care the _Val deleted
     F = _Val.
@@ -38,6 +38,31 @@ test :-
          [u,i,o,t]],
     valorEnPos(5,5,L,X,F), %F tiene el valor en esa posicion, primeras
     writeln(F).
+
+
+
+
+
+
+%mover(Pieza,I,J,NuevoI,NuevoJ).
+mover([H|T],I,J,I2,0):-size(H,Tam),J is Tam-1,I2 is I+1.
+mover(Pieza,I,J,I,J2):-J2 is J+1.
+
+
+%posicionarPieza(Matriz,I1,J1,Pieza,I2,J2).
+posicionarPieza(Matriz,I1,J1,Pieza,I2,J2):-size(Pieza,Tam),I2 is Tam.
+
+posicionarPieza(Matriz,I1,J1,Pieza,I2,J2):-valorEnPos(I1,J1,Matriz,V1),
+	valorEnPos(I1,J1,Pieza,V2),V2==x,V1==x,mover(Pieza,I2,J2,NuevoI2,NuevoJ2),
+	A is I1+NuevoI2-I2,B is J1+NuevoJ2-J2,writeln(A+B+V1+V2),
+	posicionarPieza(Matriz,A,B,Pieza,NuevoI2,NuevoJ2).
+
+posicionarPieza(Matriz,I1,J1,Pieza,I2,J2):-valorEnPos(I1,J1,Matriz,V1),
+	valorEnPos(I1,J1,Pieza,V2),V2==o,mover(Pieza,I2,J2,NuevoI2,NuevoJ2),
+	A is I1+NuevoI2-I2,B is J1+NuevoJ2-J2,writeln(A+B+V1+V2),
+	posicionarPieza(Matriz,A,B,Pieza,NuevoI2,NuevoJ2).
+
+
 
 
 
