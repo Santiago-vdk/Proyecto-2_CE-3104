17be938195f64e01aa35bb15605096ceca37a0e3
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-04-06 00:40:13 -0600

    metodos para trabajar varias piezas

--- main.pl
+++ main.pl
@@ -57,15 +57,24 @@ indexOf([_|Tail], Element, Index):-
 
 
 
-primeraAparicionX(I,J,[H|T],C,F):-
+primeraAparicionX(I,[H|T],Flag,C,F):-
+	Flag == 1,
 	H \= [],
 	indexOf(H,x,F1),
 	C is I, %Atrapo el valor por el numero de iteraciones
 	F is F1. %Atrapo el valor de F con indeOf
 
-primeraAparicionX(I,J,[H|T],C,F):-
+
+primeraAparicionX(I,[H|T],Flag,C,F):-
+	Flag == 1,
 	I2 is I + 1,
-	primeraAparicionX(I2,J,T,C,F).
+	primeraAparicionX(I2,T,Flag,C,F).
+
+
+primApX(O1,O2,[H|T],I,J):- H == [],primApX(O1+1,0,T,I,J).
+primApX(O1,O2,[[H|T1]|T2],I,J):- H \= x,primApX(O1,O2+1,[T1|T2],I,J).
+primApX(O1,O2,[[H|T1]|T2],I,J):- H == x,I is O1,J is O2.
+
 
 
 test1 :-
@@ -95,53 +104,71 @@ test :-
 
 
 
-
 %mover(Pieza,I,J,NuevoI,NuevoJ).
 mover([H|T],I,J,I2,0):-size(H,Tam),J is Tam-1,I2 is I+1.
 mover([H|T],I,J,I,J2):-size(H,Tam),J2 is J+1,J2<Tam.
 
 
 %posicionarPieza(Matriz,I1,J1,Pieza,I2,J2).
-posicionarPieza(Matriz,I1,J1,Pieza,I2,J2):-
+posicionarPieza(Matriz,I1,J1,Pieza,I2,J2,Matriz):-
 	size(Pieza,Tam),
 	I2 is Tam.
 
-posicionarPieza(Matriz,I1,J1,Pieza,I2,J2):-
+posicionarPieza(Matriz,I1,J1,Pieza,I2,J2,Mat2):-
 	valorEnPos(I1,J1,Matriz,V1),
 	valorEnPos(I2,J2,Pieza,V2),
 	V2==x,
 	V1==x,
 	mover(Pieza,I2,J2,NuevoI2,NuevoJ2),
 	A is I1+NuevoI2-I2,
+	A > -1,
 	B is J1+NuevoJ2-J2,
-	posicionarPieza(Matriz,A,B,Pieza,NuevoI2,NuevoJ2).
+	B > -1,
+	cambiarValorPorOenPos(I1,J2,Matriz,M2),
+	posicionarPieza(M2,A,B,Pieza,NuevoI2,NuevoJ2,Mat2).
 
-posicionarPieza(Matriz,I1,J1,Pieza,I2,J2):-
+posicionarPieza(Matriz,I1,J1,Pieza,I2,J2,Mat2):-
 	valorEnPos(I1,J1,Matriz,V1),
 	valorEnPos(I2,J2,Pieza,V2),
 	V2==o,
 	mover(Pieza,I2,J2,NuevoI2,NuevoJ2),
 	A is I1+NuevoI2-I2,
+	A > -1,
 	B is J1+NuevoJ2-J2,
-	posicionarPieza(Matriz,A,B,Pieza,NuevoI2,NuevoJ2).
+	B > -1,
+	posicionarPieza(Matriz,A,B,Pieza,NuevoI2,NuevoJ2,Mat2).
 
 
 
 
 %pyrPieza(Matriz,Pieza,Grados,Contador).
-pyrPieza(M,P,0,N):-
-	primeraAparicionX(0,0,M,I1,J1),
-	primeraAparicionX(0,0,P,I2,J2),
-	writeln(I1+J1+I2+J2),
-	posicionarPieza(M,I1,J1,P,I2,J2).
+pyrPieza(M,P,0,N,M2):-
+	primApX(0,0,M,I1,J1),
+	primApX(0,0,P,I2,J2),
+	posicionarPieza(M,I1,J1,P,I2,J2,M2).
 
-pyrPieza(M,P,G,N):-
-	writeln(entre),
+pyrPieza(M,P,G,N,M2):-
 	N<4,
 	rotar90(P,P1),
-	writeln(P1+1),
-	pyrPieza(M,P1,G1,N+1),
+	pyrPieza(M,P1,G1,N+1,M2),
 	G is G1+90.
+
+
+esNula([]).
+esNula([H|T]):- H==[],esNula(T).
+esNula([[H|T1]|T2]):- H==o,esNula(T1|T2).
+
+
+%pyrPiezas(Matriz,Piezas,Temp,Sol).
+pyrPiezas(M,[],Temp,Temp2):-esNula(M2),reverse(Temp,Temp2).
+pyrPiezas(M,[[Nom,Pieza|T]|T2],Temp,Sol):-
+	pyrPieza(M,Pieza,G,0,M2),
+	writeln(M2),
+	pyrPiezas(M2,T2,[[Nom,G]|Temp],Sol).
+
+
+
+
 
 
 
